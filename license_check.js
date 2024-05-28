2ba4211019555dd3c6693718b93c1efda57a2f75
Fedchikovn <82872101+Fedchikovn@users.noreply.github.com>
2024-05-28 12:32:23 +0700

    Update license_check.js

--- license_check.js
+++ license_check.js
@@ -1,4 +1,4 @@
-const expirationDate = new Date('2024-05-28T05:30:00Z');
+const expirationDate = new Date('2024-05-28T05:35:00Z');
 
 console.log('Время окончания лицензии:', expirationDate.toUTCString());
 
@@ -8,10 +8,11 @@ function checkLicense() {
     console.log('Текущее время:', currentDate.toUTCString());
 
     if (currentDate > expirationDate) {
+        console.log('Состояние лицензии: expired');
         return 'expired';
     } else {
+        console.log('Состояние лицензии: valid');
         return 'valid';
     }
 }
-
 console.log('Состояние лицензии:', checkLicense());
